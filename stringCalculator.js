fb8a7e486756eb24de3fd49dcccdd3e905034988
Aarya1402 <124037849+Aarya1402@users.noreply.github.com>
2025-07-05 18:19:43 +0530

    FEAT: return the number if only one number is provided

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,5 @@
 function add(numbers) {
     if (numbers === "") return 0; // If the input is an empty string, return 0
-    
+    if( numbers.length === 1) return parseInt(numbers, 10); // If only one number is provided, return it as an integer
 }
 module.exports = add;
