a16d5ccfe39c53594ba37fe956a49a15c6b2144a
Aarya1402 <124037849+Aarya1402@users.noreply.github.com>
2025-07-05 18:28:40 +0530

    FEAT: handle single multi-digit number without comma

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,11 @@
 function add(numbers) {
     if (numbers === "") return 0; // If the input is an empty string, return 0
-    if( numbers.length === 1) return parseInt(numbers, 10); // If only one number is provided, return it as an integer
-    if(numbers.includes(",")) {
+    if (numbers.length === 1) return parseInt(numbers, 10); // If only one number is provided, return it as an integer
+    if (numbers.includes(",")) {
         const numArray = numbers.split(",").map(num => parseInt(num, 10));
         return numArray.reduce((sum, num) => sum + num, 0); // Sum the array of numbers
-    }  
+    }
+    if (!numbers.includes(',')) return parseInt(numbers, 10);
+
 }
 module.exports = add;
