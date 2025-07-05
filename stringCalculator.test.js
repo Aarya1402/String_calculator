c8873bb1c28839d0ccb650e15b1b1848c874cee8
Aarya1402 <124037849+Aarya1402@users.noreply.github.com>
2025-07-05 18:29:25 +0530

    TEST: sum multiple comma-separated numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,7 +1,7 @@
 const add = require('./stringCalculator');
 test('Jest is working', () => {
-   expect(1).toBe(1);
-   
+    expect(1).toBe(1);
+
 });
 
 test('add function returns 0 for empty string', () => {
@@ -9,14 +9,18 @@ test('add function returns 0 for empty string', () => {
 });
 
 test('returns the number if only one number is provided', () => {
-  expect(add("4")).toBe(4);
+    expect(add("4")).toBe(4);
 });
 
 test('returns sum of two comma-separated numbers', () => {
-  expect(add("1,2")).toBe(3);
+    expect(add("1,2")).toBe(3);
 });
 
 test('returns the number if input is a single two-digit number', () => {
-  expect(add("42")).toBe(42);
+    expect(add("42")).toBe(42);
 });
+test('returns sum of multiple numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
+});
+
 
